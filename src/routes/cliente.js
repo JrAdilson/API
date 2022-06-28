d115ae38a05513fc54fe0c293c2722c734f16365
JrAdilson <imjrgonc@gmail.com>
2022-06-28 19:58:43 -0300

    docs finish

--- src/routes/cliente.js
+++ src/routes/cliente.js
@@ -2,10 +2,6 @@ const clienteController = require('../controllers/cliente')
 
 module.exports = (app) => {
     app.get('/cliente', clienteController.getCliente),
-    
-    /* #swagger.tags = ['Cliente']
-       #swagger.sumary = 'Listagem de todos os clientes cadastrados.'
-    */
     app.post('/cliente', clienteController.postCliente)
     app.delete('/cliente/:id', clienteController.deleteCliente)
     app.put('/cliente/:id', clienteController.putCliente)
